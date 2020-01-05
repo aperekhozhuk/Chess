45f5f49c8777ffa837c4fc528534dd9266120789
Anton Perekhozhuk <aperekhozhuk@heliostech.fr>
2020-01-05 03:59:32 +0000

    Add basic architecture of program

---
+++ Chess.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+class Board:
+    def __init__(self):
+        # Window settings
+        self.root = tk.Tk()
+        self.root.resizable(0,0)
+        self.root.title('Chess')
+        # Size of window - 80% percent of least dimension of screen
+        self.size = 8 * (min(self.root.winfo_screenheight(), self.root.winfo_screenwidth()) // 10)
+        # Size of cells
+        self.cellSize = self.size // 8
+        self.canvas = tk.Canvas(width = self.size, height = self.size)
+        self.canvas.pack()
+        self.canvas.bind('<Button-1>',self.Click())
+        self.root.bind('<Return>',self.NewGameEventHandler())
+        # Label for showing message about result of game
+        self.alert = None
+        # Colors for cells background
+        self.cellBg = ('#f9efeb','#c65c39')
+        # Drawing Board
+        self.drawBoard()
+        # Starting first game
+        self.NewGame()
+        # Starting window process
+        self.root.mainloop()
+    
+    # Draws all cells. Actually runs only 1 timer per program executing
+    def drawBoard(self):
+        for i in range(8):
+            for j in range(8):
+                x1 = self.cellSize * i
+                y1 = self.cellSize * j
+                x2 = x1 + self.cellSize
+                y2 = y1 + self.cellSize
+                self.canvas.create_rectangle(x1,y1,x2,y2,fill = self.cellBg[(i + j)%2])
+    
+    # Invokes, when user clicks on board
+    def Click(self):
+        def _Click(event):
+            i, j = self.GetCell(event.x, event.y)
+            print(i,j)
+        return _Click
+    
+    # Logic for restarting or running first game
+    def NewGame(self, mode = 1): 
+        pass
+
+    # Wrapper, event handler, that runs NewGame function, when Enter pressed
+    def NewGameEventHandler(self):
+        def _NewGame(event):
+            self.NewGame()
+        return _NewGame
+
+    # Returns coordinates of cell by coordinates of cursor
+    def GetCell(self,x,y):
+        i = x // self.cellSize
+        j = y // self.cellSize
+        return i, j
+
+Board = Board()
